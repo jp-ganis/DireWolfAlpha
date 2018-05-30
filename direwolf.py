d2a9fe2457a1b4291434d3e318c6aceff5f5487f
James Ganis <james.ganis@gmail.com>
2018-05-30 17:04:40 +0100

    added new things to direwolf ids

--- direwolf.py
+++ direwolf.py
@@ -18,8 +18,8 @@ frostbolt_id='CS2_024'
 
 cards = [penguin_id, river_croc_id, magma_id, yeti_id, frostbolt_id]
 cards = [penguin_id for _ in range(3)]
-og_deck = [penguin_id, river_croc_id, magma_id, yeti_id]
-og_deck_names = ["snowflipper penguin", "river crocolisk", "magma rager", "chillwind yeti"]
+og_deck = [penguin_id, river_croc_id, magma_id, yeti_id, dire_wolf_alpha_id, 'EX1_015','CS2_122','EX1_162','EX1_085','CS2_122','CS2_222','CS2_124','EX1_593','CS2_119','GVG_044','DS1_055','CS2_200']
+og_deck_names = ["snowflipper penguin", "dire wolf alpha", "river crocolisk", "magma rager", "chillwind yeti"]
 
 def setup_game():
 	from fireplace.game import Game
@@ -48,22 +48,18 @@ def setup_game():
 if __name__ == '__main__':
 	from fireplace.player import Player
 	from fireplace.game import Game
-	import copy 
-	game = setup_game()
-	gc = copy.deepcopy(game)
 	
-	player = game.players[0]
-	
-	for card in player.hand:
-		if card.is_playable():
-			card.play()
-			break
-			
-	row = getPlayerRow(player)
-	
-	board = np.zeros((2,len(row)))
-	board[0] = row
-	board[1] = row
-	board[0][30] = 3
-	board[0][32] = 5
+	print(fireplace.cards.filter(name="Novice Engineer"))
+	print(fireplace.cards.filter(name="Raid Leader"))
+	print(fireplace.cards.filter(name="Dire Wolf Alpha"))
+	print(fireplace.cards.filter(name="Mind Control Tech"))
 
+	print(fireplace.cards.filter(name="Raid Leader"))
+	print(fireplace.cards.filter(name="Stormwind Champion"))
+	print(fireplace.cards.filter(name="Wolfrider"))
+	print(fireplace.cards.filter(name="Nightblade"))
+
+	print(fireplace.cards.filter(name="Oasis Snapjaw"))
+	print(fireplace.cards.filter(name="Spider Tank"))
+	print(fireplace.cards.filter(name="Darkscale Healer"))
+	print(fireplace.cards.filter(name="Boulderfist Ogre"))
